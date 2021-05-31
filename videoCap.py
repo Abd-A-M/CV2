d7aef3fe3f340b988b8fe6d6b7e626ff4b24f98f
Abd Melhem <34129256+Abd-A-M@users.noreply.github.com>
2021-05-31 10:54:57 +0300

    Create videoCap.py

---
+++ videoCap.py
@@ -0,0 +1,20 @@
+import cv2
+# capture frames
+cap = cv2.VideoCapture(0)
+#set video type
+res = cv2.VideoWriter_fourcc(*"XVID")
+write = cv2.VideoWriter("newVideo.avi", res,60.0,(640,480))
+while True:
+    ret ,frame = cap.read()
+    if ret == True:
+        print(cap.get(cv2.CAP_PROP_FPS))
+        print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        #write frames using object 
+        write.write(frame)
+        gray = cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY)
+        cv2.imshow("frame",gray)
+        if cv2.waitKey(1) == ord("z"):
+            cv2.destroyAllWindows()
+            break
+cap.release()
